1e372a61f9162a255bf78a803ab5c93bf16e5078
Ricky <w_ziwei2004@163.com>
2026-03-14 22:39:37 +0800

    feat: 切点表达式

---
+++ demo-spring/src/main/java/org/springframework/aop/Pointcut.java
@@ -0,0 +1,12 @@
+package org.springframework.aop;
+
+
+/**
+ * 切点抽象
+ */
+public interface Pointcut {
+
+	ClassFilter getClassFilter();
+
+	MethodMatcher getMethodMatcher();
+}
--- demo-spring/src/test/java/org/springframework/test/ioc/PrototypeBeanTest.java
+++ demo-spring/src/test/java/org/springframework/test/ioc/PrototypeBeanTest.java
@@ -2,7 +2,7 @@ package org.springframework.test.ioc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.ioc.bean.Car;
+import org.springframework.test.bean.Car;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
